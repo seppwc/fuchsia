fb87223ce9825762711ee2057833bc7f83ef13d6
Sepp Wikramaratna-Clarke <Phl3bas87@gmail.com>
2020-09-24 10:32:09 +0100

    test: h - updated mock function - simplifed prop

--- packages/core/__tests__/h.spec.ts
+++ packages/core/__tests__/h.spec.ts
@@ -1,9 +1,9 @@
 import { h } from '../h';
 
 const mockFunction = jest.fn((a, b) => [a, b]);
-h(mockFunction, { propOne: 1 }, [
-  h(mockFunction, { propTwo: 2 }, 'hello'),
-  h(mockFunction, { propThree: 3 }, 'goodbye'),
+h(mockFunction, { prop: 1 }, [
+  h(mockFunction, { prop: 2 }, 'hello'),
+  h(mockFunction, { prop: 3 }, 'goodbye'),
 ]);
 
 describe('h pragma', () => {
