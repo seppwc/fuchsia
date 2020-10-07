e3adc522fdb90bc0bc45476b7e2b6f1b94e1582f
Sepp Wikramaratna-Clarke <Phl3bas87@gmail.com>
2020-10-07 11:55:25 +0100

    fix: route uses HTTP constant

--- packages/common/Route.ts
+++ packages/common/Route.ts
@@ -1,18 +1,19 @@
 import { Request } from 'express';
+import { HTTP_METHODS } from './constants';
 
 export interface IRouteProps {
-  method?: string;
+  method?: HTTP_METHODS | string;
   path?: string;
   callback: (req: Request) => Promise<unknown>;
 }
 
 export class Route {
-  public method: string;
+  public method: HTTP_METHODS | string;
   public path: string;
   public callback: (req: Request) => Promise<unknown>;
 
   constructor(public props: IRouteProps) {
-    this.method = props.method || 'GET';
+    this.method = props.method || HTTP_METHODS.GET;
     this.path = props.path || '/';
     this.callback = props.callback;
   }
