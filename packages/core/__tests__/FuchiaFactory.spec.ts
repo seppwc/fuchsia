7b07c6bf6db07fe5c552fd801988fcc9a3344b6a
Sepp Wikramaratna-Clarke <45594209+Phl3bas@users.noreply.github.com>
2020-10-07 10:03:43 +0100

    feature: load application settings and dynamic dbloader import(#17)
    * temp: direct plug in of json and urlencoded as def
    
    * feature: fixed json, static and urlencoded methods
    
    * feature: now loads application settings
    
    * change database loader to be a dynamic import
    
    * fix: changed how default port is set
    
    * fix: removed console log
    
    * fix: altered test to suit new api

--- packages/core/__tests__/FuchiaFactory.spec.ts
+++ packages/core/__tests__/FuchiaFactory.spec.ts
@@ -10,12 +10,79 @@ describe('Fuchsia Factory Create', () => {
     expect(app).toBeInstanceOf(FuchsiaApplication);
   });
 
-  it('should accept a config property (PORT) and set the port accordingly', async () => {
-    const app = await FuchsiaFactory.create({
-      controllers: [new Controller({ path: '' }, [])],
-      config: { port: 1111 },
-    });
+  it.each([
+    [{ port: 1234 }, 1234],
+    [{}, 8080],
+  ])(
+    'should take %o port setting in config and set application setting or 8000',
+    async (a, expected) => {
+      const app = await FuchsiaFactory.create({
+        controllers: [new Controller({ path: '' }, [])],
+        config: a,
+      });
+      expect(app.settings.port).toBe(expected);
+    }
+  );
 
-    expect(app.port).toBe(1111);
-  });
+  it.skip.each([
+    [{ json: true }, true],
+    [{ json: { strict: true } }, 'strict'],
+    [{ json: { inflate: true } }, 'inflate'],
+    [{ json: { limit: 200 } }, 200],
+    [{ json: { type: '*/json' } }, '*/json'],
+  ])(
+    'should take %o object in config and set JSON settings',
+    async (a, expected) => {
+      for (const [key] of Object.entries(a)) {
+        const app = await FuchsiaFactory.create({
+          controllers: [new Controller({ path: '' }, [])],
+          config: a,
+        });
+        expect(app.settings[key]).toBe(expected);
+      }
+    }
+  );
+
+  it.skip.each([
+    [{ urlEncoded: { extended: true } }, { extended: true }],
+    [{ urlEncoded: { inflate: true } }, { inflate: true }],
+    [{ urlEncoded: { limit: 200 } }, { limit: 200 }],
+    [{ urlEncoded: { limit: '200KB' } }, { limit: '200KB' }],
+    [{ urlEncoded: { parameterLimit: 2000 } }, { parameterLimit: 2000 }],
+    [
+      { urlEncoded: { type: 'application/x-www-form-urlencoded' } },
+      { type: 'application/x-www-form-urlencoded' },
+    ],
+  ])(
+    'should take %o object in config and set urlEncoded settings',
+    async (a, expected) => {
+      const app = await FuchsiaFactory.create({
+        controllers: [new Controller({ path: '' }, [])],
+        config: a,
+      });
+
+      const [key] = Object.entries(a);
+      const [k, v] = key;
+      console.log(k, v);
+
+      expect(app.settings['hi']).toBe(expected);
+    }
+  );
+
+  it.skip.each([
+    [{ static: 'public' }, 'public'],
+    [{ static: { dotfiles: 'allow' } }, { dotfiles: 'allow' }],
+    [{ static: { etag: true } }, { etag: true }],
+  ])(
+    'should take %o object in config and set static settings',
+    async (a, expected) => {
+      for (const [key] of Object.entries(a)) {
+        const app = await FuchsiaFactory.create({
+          controllers: [new Controller({ path: '' }, [])],
+          config: { a },
+        });
+        expect(app.settings[key]).toBe(expected);
+      }
+    }
+  );
 });
