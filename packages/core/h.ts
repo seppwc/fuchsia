b1e85b9986cf8ee2b4c1aceb903d5943cf0dca88
Sepp Wikramaratna-Clarke <Phl3bas87@gmail.com>
2020-10-07 10:27:04 +0100

    fix: declare JSX globally rename h to JSX

--- packages/core/h.ts
+++
@@ -1,7 +0,0 @@
-export const h = (name: any, props: {}, ...children: any[]): any => {
-  if (typeof name === 'function' && name.toString().indexOf('class') === 0) {
-    return new name(props, children);
-  } else {
-    return name(props, children);
-  }
-};
