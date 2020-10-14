c41a16dbf52e89e53efc02f48d711e9fab834550
phl3bas <phl3bas87@gmail.com>
2020-10-14 16:23:04 +0100

    feature(templates): implement render prop on route

--- packages/common/Controller.ts
+++ packages/common/Controller.ts
@@ -1,6 +1,7 @@
-import { Router, Request } from 'express';
-import { Route } from './Route';
 import { HTTP } from './constants';
+import { Router, Request, Response } from 'express';
+import { Route } from './Route';
+
 
 interface IControllerProps {
   path: string;
@@ -16,45 +17,19 @@ export class Controller {
     this.handleRoutes();
   }
 
+
   private async handleRoutes(): Promise<void> {
     this.children.forEach((child: Route) => {
-      switch (child.method.toUpperCase()) {
-        case HTTP.GET:
-          this.router.get(child.path, async (req: Request, res) => {
-            const response = await child.callback(req);
-            res.send(response);
-          });
-          break;
-        case HTTP.POST:
-          this.router.post(child.path, async (req: Request, res) => {
-            const response = await child.callback(req);
-            res.send(response);
-          });
-          break;
-        case HTTP.PUT:
-          this.router.put(child.path, async (req: Request, res) => {
-            const response = await child.callback(req);
-            res.send(response);
-          });
-          break;
-        case HTTP.PATCH:
-          this.router.patch(child.path, async (req: Request, res) => {
-            const response = await child.callback(req);
-            res.send(response);
-          });
-          break;
-        case HTTP.DELETE:
-          this.router.delete(child.path, async (req: Request, res) => {
-            const response = await child.callback(req);
-            res.send(response);
-          });
-          break;
-        default:
-          this.router.all(child.path, async (req: Request, res) => {
-            const response = await child.callback(req);
-            res.send(response);
-          });
-      }
+        this.router[child.method.toLowerCase()](child.path, async (req: Request, res: Response ) => {
+          const response = await child.callback(req);
+          if(child.json) {
+            res.json(response)
+          } else if(child.render) {
+            res.render(child.render, response as object)
+          } else {
+            res.send(response)
+          }
+        });
     });
   }
 }
--- packages/common/Route.ts
+++ packages/common/Route.ts
@@ -4,17 +4,23 @@ import { HTTP } from './constants';
 export interface IRouteProps {
   method?: HTTP | string;
   path?: string;
+  render?: string | false
+  json?: boolean
   callback: (req: Request) => Promise<unknown>;
 }
 
 export class Route {
   public method: HTTP | string;
   public path: string;
+  public render: string | false;
+  public json: boolean
   public callback: (req: Request) => Promise<unknown>;
 
   constructor(public props: IRouteProps) {
     this.method = props.method || HTTP.GET;
     this.path = props.path || '/';
     this.callback = props.callback;
+    this.render = props.render || false;
+    this.json = props.json || false;
   }
 }
